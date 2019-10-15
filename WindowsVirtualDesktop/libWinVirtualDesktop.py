0e0f36ce6cf86a14980076e641015831cefc7e68
Kevin Schlosser <kdschlosser@users.noreply.github.com>
2019-10-14 19:20:01 -0600

    Update libWinVirtualDesktop.py

--- WindowsVirtualDesktop/libWinVirtualDesktop.py
+++ WindowsVirtualDesktop/libWinVirtualDesktop.py
@@ -1,7 +1,16 @@
 def __bootstrap__():
-    global __bootstrap__, __loader__, __file__
-    import sys, pkg_resources, imp
-    __file__ = pkg_resources.resource_filename(__name__, 'libWinVirtualDesktop.pyd')
-    __loader__ = None; del __bootstrap__, __loader__
-    imp.load_dynamic(__name__,__file__)
+    global __bootstrap__
+    global __loader__
+    global __file__
+    import sys
+    import imp
+    __file__ += 'd'
+    
+    del __bootstrap__
+    del __loader__
+    
+    mod = imp.load_dynamic(__name__, __file__)
+    
+    sys.modules[__name__] = mod
+    
 __bootstrap__()
